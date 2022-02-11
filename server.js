514775aab770161029c9970f62fa62b25d57ad07
JohnRamberger <jramberger3@gatech.edu>
2022-02-11 16:59:15 -0500

    change server port
    changed port from 5069 to 3000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 5069;
+const port = 3000;
 
 //load other things
 const generateApiKey = require("generate-api-key");
