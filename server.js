069e20d913524bdd80fed4ef2d4fabb33ca96d1a
JohnRamberger <jramberger3@gatech.edu>
2022-02-11 17:02:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const port = 5000;
 
 //load other things
 const generateApiKey = require("generate-api-key");
@@ -348,6 +349,6 @@ app.get("/api/:key/", async (req, res) => {
 */
 
 //---------------end of requests----------
-app.listen(process.env.PORT || 5000, () =>
+app.listen(process.env.PORT || port, () =>
   console.log(`Hello world app listening on port ${port}!`)
 );
