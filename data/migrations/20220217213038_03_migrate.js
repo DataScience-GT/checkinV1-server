ea8c36dd3902c84fef9058ab8b025061f94d8205
JohnRamberger <jramberger3@gatech.edu>
2022-02-17 17:57:31 -0500

    Update 20220217213038_03_migrate.js

--- data/migrations/20220217213038_03_migrate.js
+++ data/migrations/20220217213038_03_migrate.js
@@ -1,11 +1,11 @@
 exports.up = (knex) => {
   return knex.schema
-    .dropTable("api_keys")
-    .dropTable("events")
-    .dropTable("login")
-    .dropTable("users")
-    .dropTable("session")
-    .dropTable("checkin-template")
+    .dropTableIfExists("api_keys")
+    .dropTableIfExists("events")
+    .dropTableIfExists("login")
+    .dropTableIfExists("users")
+    .dropTableIfExists("session")
+    .dropTableIfExists("checkin-template")
     .createTable("api_keys", function (table) {
       table.increments("id").primary();
       table.string("name", 255);
@@ -48,10 +48,10 @@ exports.up = (knex) => {
 
 exports.down = (knex) => {
   return knex.schema
-    .dropTable("api_keys")
-    .dropTable("events")
-    .dropTable("login")
-    .dropTable("users")
-    .dropTable("session")
-    .dropTable("checkin-template");
+    .dropTableIfExists("api_keys")
+    .dropTableIfExists("events")
+    .dropTableIfExists("login")
+    .dropTableIfExists("users")
+    .dropTableIfExists("session")
+    .dropTableIfExists("checkin-template");
 };
