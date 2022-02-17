690ed5221cb26fc97a806cd2056b7d00b0477000
JohnRamberger <jramberger3@gatech.edu>
2022-02-17 17:58:42 -0500

    Update 20220217174425_first-migration.js

--- data/migrations/20220217174425_first-migration.js
+++ data/migrations/20220217174425_first-migration.js
@@ -1,5 +1,11 @@
 exports.up = (knex) => {
   return knex.schema
+    .dropTableIfExists("api_keys")
+    .dropTableIfExists("events")
+    .dropTableIfExists("login")
+    .dropTableIfExists("users")
+    .dropTableIfExists("session")
+    .dropTableIfExists("checkin-template")
     .createTable("api_keys", function (table) {
       table.increments("id").primary();
       table.string("name", 255);
@@ -35,14 +41,13 @@ exports.up = (knex) => {
     .createTable("checkin-template", function (table) {
       table.bigInteger("barcodeNum", 255).unique();
       table.integer("attended", 255).notNullable().defaultTo(0);
-      table.string("lastModified", 255);
       table.string("modifiedBy", 255);
+      table.timestamps(true, true);
     });
 };
 
 exports.down = (knex) => {
   return knex.schema
-    .dropTableIfExists("profiles")
     .dropTableIfExists("api_keys")
     .dropTableIfExists("events")
     .dropTableIfExists("login")
